d6d12504e69d84c28220391c9ba15bc79924321a
kamit17 <kamit17@outlook.com>
2016-02-17 21:32:30 -0600

    Program executes in Macbook but not on Imac

---
+++ CS50/reverse_digits.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+    int number, right_digit;
+    printf("Enter the number.\n");
+    scanf("%i",&number);
+
+    while(number !=0)
+    {
+        right_digit=number%10;
+        printf("%i",right_digit);
+        number=number/10;
+    }
+
+    printf("\n");
+
+    return 0;
+}
