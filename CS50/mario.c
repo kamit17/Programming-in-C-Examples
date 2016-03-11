6d33f28c0a57e5c9b87a4afa1f4fb010bcc3a8d6
kamit17 <kamit17@outlook.com>
2016-03-10 23:06:57 -0600

    cs50

---
+++ CS50/mario.c
@@ -0,0 +1,40 @@
+
+/*Write a program to print th following pattern*/
+
+/*
+    *
+   **
+  ***
+ ****
+*****    */
+
+#include<stdio.h>
+int main()
+{
+    int height=0,row,col1,col2,noOfCols;
+    // noOfrows=the no of rows
+    //noOfRows=5;
+    do{
+    printf("Height: ");
+    scanf("%i",&height);
+    noOfCols=height-1;
+    }
+    while(height >23 || height<=0);
+
+    for(row = 1; row<=height ; row++)
+    {
+         for(col1 = 1; col1 <= noOfCols ; col1++)
+         {
+         printf(" ");
+         }
+         noOfCols--;
+         for(col2 =0; col2 <= row; col2++)
+         {
+         printf("#");
+         }
+         printf("\n");
+    }
+
+
+
+}
---
+++ CS50/mario1.c
@@ -0,0 +1,42 @@
+
+/*Write a program to print th following pattern*/
+
+/*
+    *
+   **
+  ***
+ ****
+*****    */
+
+/*The logic is that we need to have three loops. One for iterating through th column. One for iterating through the rows and printing  space and the third for iterating through the loops to print #*/
+
+#include<stdio.h>
+int main()
+{
+    int height=0,col,row1,row2,noofrows;
+    // noOfrows=the no of rows
+    //noOfRows=5;
+    do{
+    printf("Height: ");  //do while loop to get the height
+    scanf("%i",&height);
+    noofrows=height-1;   //for the gap
+    }
+    while(height >23 || height<=0);
+
+    for(col = 2; col <= height ; col++)   //outerloop for the columns
+    {
+         for(row1 = 2; row1 <= noofrows ; row1++)
+         {
+         printf(" ");
+         }
+         noofrows--;
+         for(row2 =1 ; row2 <= col; row2++)
+         {
+         printf("#");
+         }
+         printf("\n");
+    }
+
+
+
+}
