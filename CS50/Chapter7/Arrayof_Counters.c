5ff0f5edcd25468b100cbcda0d4b946f71d9e276
kamit17 <kamit17@outlook.com>
2016-03-27 19:56:45 -0500

    Arrays

---
+++ CS50/Chapter7/Arrayof_Counters.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main (void)
+{
+    int ratingCounters[11],i,response;
+
+    for( i = 1; i<=10; ++i)
+        ratingCounters[i]=0;
+
+    printf("enter your response");
+
+    for(i=1; i<=20;++i)
+    {
+        scanf("%i",&response);
+
+        if(response <1 || response > 10)
+            printf("Bad response: %i\n",response);
+
+        else
+        ++ratingCounters[response];
+
+    }
+
+    printf("\n\nRating Number of Responses\n");
+    printf("------ -----------------\n");
+
+    for( i =1; i<=10 ;++i)
+        printf("%4i%4i%41\n",i,ratingCounters[i]);
+
+    return 0;
+}
---
+++ CS50/Chapter7/primenumber.c
@@ -0,0 +1,33 @@
+/*The expression p/primes[i] >=primes[i] is used in the innermoset for loop to ensure that the value of p does not exceed the sqaure root of primes[i]*/
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int main (void)
+{
+  int p,i, primes[50], primeIndex = 2; //primeIndex is the next free slot in the primes Array
+  bool isPrime;
+
+  primes[0]=2;
+  primes[1] = 3;
+
+  for(p=5; p <= 50; p = p + 2) //loop for running through odd integers from 5 to 50 .
+  {
+    int isPrime = true; //Boolean variable set to 0
+
+    for (i = 1; isPrime && p / primes[i] >= primes[i]; ++i) // Successively divides the value of p by all the previously genreated prime numbers that are stored in the prime Array
+      if( p % primes[i] == 0)
+          isPrime = false ;
+
+      if( isPrime == true)
+      {
+        primes[primeIndex] = p;
+        ++primeIndex;
+      }
+    }
+      for(i=0; i< primeIndex ; ++i)
+        printf("%i ",primes[i] );
+        printf("\n");
+
+        return 0;
+  }
