213e1cd1d047a7d28f296e97c56119df585e5ed6
kamit17 <kamit17@outlook.com>
2016-04-19 21:41:49 -0500

    Chapter8

---
+++ CS50/Programming_in_C/Chapter8/Newton_Raphson.c
@@ -0,0 +1,34 @@
+/*Calculating the square root of a number*/
+
+//Function to calculte the absolute value of a number
+
+#include<stdio.h>
+
+float absoluteValue (float x)
+{
+  if (x < 0)
+    x = -x;
+  return (x);
+}
+
+//Function to compute the square root of a number
+
+float squareRoot (float x)
+{
+
+  const float epsilon = .000001;
+  float   guess = 1.0;
+
+  while( absoluteValue (guess * guess - x) >= epsilon)
+      guess = (x /guess + guess) / 2.0;
+
+    return guess;
+}
+
+int main (void ) {
+  /* code */
+  printf("squareRoot (2.0) = %f\n",squareRoot (2.0));
+  printf("squareRoot (144.0) = %f\n",squareRoot (144.0));
+  printf("squareRoot (11.5) = %f\n",squareRoot (11.5));
+  return 0;
+}
