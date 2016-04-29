625375c35084025965e08718fe70a8bf38684242
kamit17 <kamit17@outlook.com>
2016-04-28 22:14:41 -0500

    Chapter8

--- CS50/Programming_in_C/Chapter8/multidimArray_fun.c
+++ CS50/Programming_in_C/Chapter8/multidimArray_fun.c
@@ -29,3 +29,26 @@ int main (void)
   return 0;
 
 }
+
+//Function to multiply a 3 X 55 array by a scalar
+
+void scalarMultiply (int matrix[3][5], int scalar)
+{
+  int row, column;
+
+  for (row = 0; row < 3; ++row )
+      for (column = 0; column < 5; ++column )
+            matrix[row][column] *= scalar;
+
+}
+
+void displayMatrix (int matrix[3][5])
+{
+  int row, column;
+      for (row = 0; row < 3; ++row)
+      {
+        for (column = 0; column < 5; ++column)
+          printf("%5i",matrix[row][column]);
+          printf("\n");
+      }
+}
