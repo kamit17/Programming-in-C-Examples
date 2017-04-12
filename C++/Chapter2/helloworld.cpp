9adbd8db8c522c417b3c3ac1a37a5ca77c13031c
kamit17 <kamit17@outlook.com>
2017-04-11 23:52:20 -0500

    deleted some files

--- C++/Chapter2/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-  cout << "HELLO, Can you hear me ?? Oh, and Hello World!\n";
-  //cin.getg();
-}
--- Web_Designing/Readme.md
+++
@@ -1,1 +0,0 @@
-This is a Repo for Web_Development Courses.
